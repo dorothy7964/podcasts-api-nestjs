fb8f8bf320a7f2e5b7dcca7151fa6b4d062042ca
wam <dorothy_007@naver.com>
2024-06-26 01:30:39 +0900

    Hashing Passwords

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,3 +1,4 @@
+import { InternalServerErrorException } from "@nestjs/common";
 import {
   Field,
   InputType,
@@ -6,7 +7,8 @@ import {
 } from "@nestjs/graphql";
 import { IsEnum, IsString } from "class-validator";
 import { CoreEntity } from "src/common/entities/core.entity";
-import { Column, Entity } from "typeorm";
+import { BeforeInsert, Column, Entity } from "typeorm";
+import * as bcrypt from "bcrypt";
 
 enum UserRole {
   HOST = "HOST",
@@ -38,4 +40,14 @@ export class User extends CoreEntity {
   @IsEnum(UserRole)
   @IsString()
   role: UserRole;
+
+  @BeforeInsert()
+  async hashPassword(): Promise<void> {
+    try {
+      this.password = await bcrypt.hash(this.password, 10);
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException();
+    }
+  }
 }
