9ecb62db59b17ccc61d9fdde881ee8300580631d
wam <dorothy_007@naver.com>
2024-06-26 18:25:56 +0900

    미들웨어 추가, 유저 토큰 복호화

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,10 @@
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
-import { Module } from "@nestjs/common";
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { GraphQLModule } from "@nestjs/graphql";
 import { TypeOrmModule } from "@nestjs/typeorm";
@@ -11,6 +16,7 @@ import { UsersModule } from "./users/users.module";
 import * as Joi from "joi";
 import { User } from "./users/entities/user.entity";
 import { JwtModule } from "./jwt/jwt.module";
+import { JwtMiddleware } from "./jwt/jwt.middleware";
 
 @Module({
   imports: [
@@ -51,4 +57,11 @@ import { JwtModule } from "./jwt/jwt.module";
     UsersModule,
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(JwtMiddleware).forRoutes({
+      path: "/graphql",
+      method: RequestMethod.POST,
+    });
+  }
+}
