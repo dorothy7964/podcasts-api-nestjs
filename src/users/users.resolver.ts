d122ab262c66af8efaa1a76623906dad003debd9
wam <dorothy_007@naver.com>
2024-07-16 03:22:35 +0900

    AuthUser 리턴 값 오류 수정

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,16 +1,16 @@
-import { Args, Context, Mutation, Query, Resolver } from "@nestjs/graphql";
-import { User } from "./entities/user.entity";
-import { UsersService } from "./users.service";
+import { UseGuards } from "@nestjs/common";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { AuthUser } from "src/auth/auth-user.decorator";
+import { AuthGuard } from "src/auth/auth.guard";
 import {
   CreateAccountInput,
   CreateAccountOutput,
 } from "./dtos/create-account.dto";
-import { LoginInput, LoginOutput } from "./dtos/login.dto";
-import { UseGuards } from "@nestjs/common";
-import { AuthGuard } from "src/auth/auth.guard";
-import { AuthUser } from "src/auth/auth-user.decorator";
-import { UserProfileInput, UserProfileOutput } from "./dtos/user-profile.dto";
 import { EditProfileInput, EditProfileOutput } from "./dtos/edit-profile.dto";
+import { LoginInput, LoginOutput } from "./dtos/login.dto";
+import { UserProfileInput, UserProfileOutput } from "./dtos/user-profile.dto";
+import { User } from "./entities/user.entity";
+import { UsersService } from "./users.service";
 
 @Resolver(() => User)
 export class UsersResolver {
