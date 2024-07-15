a65fe58e1cf27fb5bf9364cf1760eea3eddb6675
wam <dorothy_007@naver.com>
2024-07-15 19:27:02 +0900

    User E2E, 테스트가 끝난 후 데이터베이스 내용 drop하기

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,9 +1,11 @@
 import { INestApplication } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
 import { AppModule } from "src/app.module";
+import { DataSource } from "typeorm";
 
 describe("UserModule (E2E)", () => {
   let app: INestApplication;
+  let dataSource: DataSource;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,9 +13,13 @@ describe("UserModule (E2E)", () => {
     }).compile();
     app = module.createNestApplication();
     await app.init();
+
+    dataSource = module.get(DataSource);
   });
 
   afterAll(async () => {
+    await dataSource.dropDatabase();
+    await dataSource.destroy();
     app.close();
   });
 
